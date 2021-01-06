c5778eeab9f9abeffba67e0ec0aea273c043b37b
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-01-06 02:53:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ def valid?
 end 
 
 def execute_transaction
-  if  ! self.valid? || @sender.balance < @amount
+  if  !self.valid? || @sender.balance < @amount
     
     @status = "rejected" 
       return "Transaction rejected. Please check your account balance."
