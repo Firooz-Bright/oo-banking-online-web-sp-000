ce9081b4227fe1cd17075bbd978696b178bdedae
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2021-01-06 02:42:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ def execute_transaction
       return "Transaction rejected. Please check your account balance."
       elsif
        @status == "complete"
-      puts "Transaction was already excuted"
+         puts "Transaction was already excuted"
        else
       @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
